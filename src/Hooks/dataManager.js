6d8d6a7938bc584302265269dc52479755f9f4d2
NOV\jacobsend <daniel.jacobsen@nov.com>
2021-09-15 14:23:40 +0200

    trying to add timefilter on dataManager

--- src/Hooks/dataManager.js
+++ src/Hooks/dataManager.js
@@ -1,17 +1,20 @@
 
 const tdata = require("./databaseMoodle.json");
-
+//console.log(tdata);
 
 const dataManager = (searchInput) => {
     //filter data in by global seach
     const fdata = tdata.filter(e => {
+        const dateformat = new Date(e.timefinish).toLocaleDateString()
+        console.log(e.timefinish);
         if (e.name.toLowerCase().indexOf(searchInput.toLowerCase()) !== -1) { return e }
     });
-    let passingScore = 70;
-    let exams = [];
-    let examsObject = [];
-    let sorted = [];
-    const totalAttempts = fdata.length;
+    const passingScore = 80; //input variable
+    const exams = []; //temporary storage
+    const examsObject = []; //returned object
+    const sorted = []; //returned object
+    const totalAttempts = fdata.length; //returned number
+
 
 
     //modifies tdata list and adds whether its a passed or failed attempt and a percentage score
@@ -24,12 +27,25 @@ const dataManager = (searchInput) => {
             sorted.push([]);
         }
 
-    })
+    });
+
+     //returnes object with 2 arrays one being months of the year and 2 being number of attempts that month
+    const montlyAttempts =(() => {
+        let months =[];
+        let attemptsM =[];
+        for(let i=0;i<fdata.length;i++){
+           let eMonth = new Date(fdata[i].timefinish).toLocaleDateString('en-us', {  month:"long"});
+           if(months.indexOf(eMonth) === -1) months.push(eMonth);
+        }
+        return {"months":months, "attempts":attemptsM}
+      })();
+      //console.log(montlyAttempts)
+
     //sorts the tdata object into an array(sorted[]) that contains arrays of each exams results. 
     fdata.map(function (e) {
         let sortIndex = exams.indexOf(e.name);
         sorted[sortIndex].push(e);
-    })
+    });
 
     //creates the  examsObject with each exam and how many passed or failed 
     //that exam based on the passingScore variable
@@ -46,21 +62,21 @@ const dataManager = (searchInput) => {
         examsObject.push({ "name": name, "passCount": passCount, "failCount": failCount });
     }
 
-    //count up all passed/failed attempts from all exams in the examsOverview object
+    //count up all passed/failed attempts from all exams in the examsOverview object and give them to whomever is asking
     const passCount = (p) => {
-        let passStack = 0;
-        let failStack = 0;
+        let passStack = 0,
+            failStack = 0;
             for (let i = 0; i < examsObject.length; i++) {
                 passStack += examsObject[i].passCount;
                 failStack += examsObject[i].failCount;
             }
-        
-        if (p === "pass") { return passStack };
-        if (p === "fail") { return failStack };
+        if (p === "pass") return passStack ;
+        if (p === "fail") return failStack ;
     }
 
     return { "passingScore": passingScore, "examsOverview": examsObject, "examData": sorted, "totalAttempts": totalAttempts, "passCountFunction": passCount }
 }
+console.log(dataManager("electrical"))
 
-export default dataManager
+//export default dataManager
 
