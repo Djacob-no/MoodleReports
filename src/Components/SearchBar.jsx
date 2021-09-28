33c892f1a2f49482a4f343b142d116cac4f1ecaf
NOV\jacobsend <daniel.jacobsen@nov.com>
2021-09-28 16:28:09 +0200

    css changes

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 const FieldEditor = ({ onChange }) => {
 
   const [search, setSearch] = useState("");
-  const [from, setFrom] = useState("1980-01-01");
+  const [from, setFrom] = useState("2019-01-01");
   const [to, setTo] = useState("2200-01-01");
 
 
@@ -19,17 +19,16 @@ const FieldEditor = ({ onChange }) => {
     console.log({ "text": search, "from": from, "to": to })
   }
 
+const date = new Date();
+const dateString = date.getFullYear() + '-' + ('0' + (date.getMonth()+1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2);
 
   return (
     <div className="field-editor">
       <form onSubmit={onSubmit}>
         <input  id="search" className="main-search" placeholder="Search" onChange={handleChange}  />
         <div>
-          <button >Use Dates</button>
-          <div>
-            <input className="dateInput" type="date" id="from" onChange={handleChange}></input>
-            <input className="dateInput" type="date" id="to" onChange={handleChange}></input>
-          </div>
+            <input value={from} className="dateInput" type="date" id="from" onChange={handleChange}></input>-
+            <input  value={dateString} className="dateInput" type="date" id="to" onChange={handleChange}></input>
         </div>
       </form>
 
